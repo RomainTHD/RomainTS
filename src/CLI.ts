08d52e02f571e239a81ada1d48570440e406c8c7
RomainTHD <romain-thd+github@protonmail.com>
2023-07-11 21:13:40 +0200

    fix: Fix IO errors

--- src/CLI.ts
+++ src/CLI.ts
@@ -1,13 +1,20 @@
 import { program } from "commander";
 import * as fs from "fs";
+import { LoggerFactory } from "./utils/Logger";
 
 program.option("-i, --input <path>", "Input file path");
 program.parse();
 
 export module CLI {
 	const options = program.opts();
+	const logger = LoggerFactory.get("CLI");
 
 	export function getInputContent(): string {
-		return fs.readFileSync(options["input"], "utf8");
+		try {
+			return fs.readFileSync(options["input"], "utf8");
+		} catch (e: unknown) {
+			logger.error(`Couldn't read input file: ${e}`);
+			process.exit(1);
+		}
 	}
 }
