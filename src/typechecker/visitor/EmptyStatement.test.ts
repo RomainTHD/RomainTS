b4243907f7e70d2a204d2609a8d1f48a6fb445c7
Romain Theodet <romain.theodet@amersports.com>
2023-07-11 19:11:09 +0200

    feat: Add empty statements

---
+++ src/typechecker/visitor/EmptyStatement.test.ts
@@ -0,0 +1,11 @@
+import { describe, it } from "vitest";
+import { AST } from "../../AST";
+import { TypeChecker } from "../accept";
+import { Env } from "../env";
+
+describe("EmptyStatementVisitor", () => {
+	it("should work for empty statements", async () => {
+		const content = ";";
+		await TypeChecker.accept(AST.parse(content), new Env());
+	});
+});
