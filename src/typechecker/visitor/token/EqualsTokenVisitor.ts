97cb91c8f847779b849387eccd8e044c9d19bde1
RomainTHD <romain-thd+github@protonmail.com>
2023-07-17 21:28:36 +0200

    refactor: Remove hardcoded values

--- src/typechecker/visitor/token/EqualsTokenVisitor.ts
+++ src/typechecker/visitor/token/EqualsTokenVisitor.ts
@@ -6,26 +6,20 @@ import { TypecheckingFailure } from "../../TypecheckingFailure";
 
 const logger = LoggerFactory.get("EqualsTokenVisitor");
 
-const nonWritableGlobal = ["undefined", "NaN", "Infinity"];
-
-// TODO: Refactor this visitor signature
-
 export async function visit(
 	node: ts.Token<ts.SyntaxKind.EqualsToken>,
 	env: Env,
 	{ left, right }: { left: string; right: Type },
 ): Promise<Type> {
-	if (nonWritableGlobal.includes(left)) {
-		if (env.isStrictMode()) {
-			throw new TypecheckingFailure(`Cannot assign to '${left}'`, node);
-		} else {
-			logger.warn(`Suspicious assignment to '${left}'`);
-		}
-	}
-
 	const variable = env.get(left);
 	if (variable) {
-		if (!variable.isMutable) {
+		if (variable.builtin) {
+			if (env.isStrictMode()) {
+				throw new TypecheckingFailure(`Cannot assign to builtin '${left}'`, node);
+			} else {
+				logger.warn(`Suspicious assignment to builtin '${left}'`);
+			}
+		} else if (!variable.isMutable) {
 			throw new TypecheckingFailure(`Cannot assign to constant '${left}'`, node);
 		} else if (!variable.vType.contains(right)) {
 			throw new TypecheckingFailure(`Type '${right}' is not assignable to type '${variable.vType}'`, node);
