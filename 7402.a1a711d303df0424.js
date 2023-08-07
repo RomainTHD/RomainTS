846d6cba859e10f77b34aa0a579d1c4ea6ee4cae
RomainTHD <romain-thd+github@protonmail.com>
2023-08-07 15:48:07 +0000

    ci: Auto-deploy to GitHub Pages
    Triggered by commit: https://github.com/RomainTHD/RomainTS/commit/b268b8fb178caa636eea8a20fdec9c0f378828f3

---
+++ 7402.a1a711d303df0424.js
@@ -0,0 +1,79 @@
+"use strict";
+(self["webpackChunkromaints"] = self["webpackChunkromaints"] || []).push([[7402],{
+
+/***/ 7402:
+/*!**************************************************************!*\
+  !*** ../src/typechecker/visitor/token/EqualsTokenVisitor.ts ***!
+  \**************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   visit: () => (/* binding */ visit)
+/* harmony export */ });
+/* harmony import */ var _home_runner_work_RomainTS_RomainTS_demo_node_modules_babel_runtime_helpers_esm_asyncToGenerator_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./node_modules/@babel/runtime/helpers/esm/asyncToGenerator.js */ 5861);
+/* harmony import */ var ___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../.. */ 2429);
+/* harmony import */ var _types__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../types */ 2122);
+/* harmony import */ var _utils_Logger__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../../utils/Logger */ 2675);
+
+
+
+
+const logger = _utils_Logger__WEBPACK_IMPORTED_MODULE_3__.LoggerFactory.create("EqualsTokenVisitor");
+function visit(_x, _x2) {
+  return _visit.apply(this, arguments);
+}
+function _visit() {
+  _visit = (0,_home_runner_work_RomainTS_RomainTS_demo_node_modules_babel_runtime_helpers_esm_asyncToGenerator_js__WEBPACK_IMPORTED_MODULE_0__["default"])(function* (node, env) {
+    const left = env.getData("left");
+    const right = env.getData("right");
+    if (typeof left !== "string") {
+      // `0 = ...;`
+      // FIXME: Use a cleaner way to check if `left` is a LValue or a RValue
+      throw new ___WEBPACK_IMPORTED_MODULE_1__.TypecheckingFailure("The left-hand side of an assignment expression must be a variable or a property access", node);
+    }
+    const variable = env.lookup(left);
+    if (variable) {
+      if (variable.builtin) {
+        if (env.config.strictMode) {
+          throw new ___WEBPACK_IMPORTED_MODULE_1__.TypecheckingFailure(`Cannot assign to builtin '${left}'`, node);
+        } else {
+          logger.warn(`Suspicious assignment to builtin '${left}'`);
+        }
+      } else if (!variable.isMutable) {
+        throw new ___WEBPACK_IMPORTED_MODULE_1__.TypecheckingFailure(`Cannot assign to constant '${left}'`, node);
+      } else if (!variable.vType.contains(right)) {
+        throw new ___WEBPACK_IMPORTED_MODULE_1__.TypecheckingFailure(`Type '${right}' is not assignable to type '${variable.vType}'`, node);
+      }
+    } else {
+      // `x = 0` where `x` is not declared. Valid in non-strict mode
+      if (env.config.strictMode) {
+        throw new ___WEBPACK_IMPORTED_MODULE_1__.TypecheckingFailure(`Variable ${left} not found`, node);
+      } else {
+        logger.warn(`Variable '${left}' not found, declaring it`);
+        if (right instanceof _types__WEBPACK_IMPORTED_MODULE_2__.LiteralType) {
+          env.add(left, {
+            vType: right.literal.vType,
+            isLocal: false,
+            isMutable: true
+          });
+        } else {
+          env.add(left, {
+            vType: right,
+            isLocal: false,
+            isMutable: true
+          });
+        }
+      }
+    }
+    if (right instanceof _types__WEBPACK_IMPORTED_MODULE_2__.LiteralType) {
+      return right.literal.vType;
+    }
+    return right;
+  });
+  return _visit.apply(this, arguments);
+}
+
+/***/ })
+
+}]);
---
+++ 9951.6cae1b6b632c498b.js
@@ -0,0 +1,41 @@
+"use strict";
+(self["webpackChunkromaints"] = self["webpackChunkromaints"] || []).push([[9951],{
+
+/***/ 9951:
+/*!*****************************************************************************!*\
+  !*** ../src/typechecker/visitor/declaration/TypeAliasDeclarationVisitor.ts ***!
+  \*****************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   visit: () => (/* binding */ visit)
+/* harmony export */ });
+/* harmony import */ var _home_runner_work_RomainTS_RomainTS_demo_node_modules_babel_runtime_helpers_esm_asyncToGenerator_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./node_modules/@babel/runtime/helpers/esm/asyncToGenerator.js */ 5861);
+/* harmony import */ var ___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../.. */ 2429);
+/* harmony import */ var _utils_NotImplementedException__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../utils/NotImplementedException */ 7971);
+
+
+
+function visit(_x, _x2) {
+  return _visit.apply(this, arguments);
+}
+function _visit() {
+  _visit = (0,_home_runner_work_RomainTS_RomainTS_demo_node_modules_babel_runtime_helpers_esm_asyncToGenerator_js__WEBPACK_IMPORTED_MODULE_0__["default"])(function* (node, env) {
+    if (node.modifiers) {
+      throw new _utils_NotImplementedException__WEBPACK_IMPORTED_MODULE_2__.NotImplementedException();
+    }
+    const name = yield env.withChildData({
+      resolveIdentifier: false
+    }, /*#__PURE__*/(0,_home_runner_work_RomainTS_RomainTS_demo_node_modules_babel_runtime_helpers_esm_asyncToGenerator_js__WEBPACK_IMPORTED_MODULE_0__["default"])(function* () {
+      return yield ___WEBPACK_IMPORTED_MODULE_1__.TypeChecker.accept(node.name, env);
+    }));
+    const t = yield ___WEBPACK_IMPORTED_MODULE_1__.TypeChecker.accept(node.type, env);
+    env.addType(name, t);
+  });
+  return _visit.apply(this, arguments);
+}
+
+/***/ })
+
+}]);
