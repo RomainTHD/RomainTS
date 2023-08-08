bd85592b09c0936738efe8a0e6e4f1abbf43292d
Romain Theodet <romain.theodet@amersports.com>
2023-08-08 15:20:30 +0200

    fix: Prevent lexical declaration in a single-statement context

--- src/typechecker/visitor/statement/IfStatementVisitor.test.ts
+++ src/typechecker/visitor/statement/IfStatementVisitor.test.ts
@@ -1,5 +1,5 @@
-import { describe, it } from "vitest";
-import { Env, TypeChecker } from "../..";
+import { describe, expect, it } from "vitest";
+import { Env, TypeChecker, TypecheckingFailure } from "../..";
 import { AST } from "../../../AST";
 
 describe("IfStatementVisitor", () => {
@@ -10,5 +10,12 @@ describe("IfStatementVisitor", () => {
 		}
 		`;
 		await TypeChecker.accept(AST.parse(content), Env.create());
+	});
+
+	it("should not work with single statement lexical definition", async () => {
+		const content = `
+		if (true) let x;
+		`;
+		await expect(TypeChecker.accept(AST.parse(content), Env.create())).rejects.toThrowError(TypecheckingFailure);
 	});
 });
--- src/typechecker/visitor/statement/IfStatementVisitor.ts
+++ src/typechecker/visitor/statement/IfStatementVisitor.ts
@@ -1,12 +1,23 @@
-import type ts from "typescript";
+import ts from "typescript";
 import { StatementReturn, StatementVisitor } from ".";
-import { TypeChecker } from "../..";
+import { TypeChecker, TypecheckingFailure } from "../..";
 import { UnionType } from "../../../types";
 import { Bool3 } from "../../../utils/Bool3";
 
 export const visit: StatementVisitor<ts.IfStatement> = async (node, env) => {
 	// All kind of expressions are either truthy or falsy
 	await TypeChecker.accept(node.expression, env);
+
+	if (node.thenStatement.kind === ts.SyntaxKind.VariableStatement) {
+		/*
+		Avoid the following illegal code:
+		```ts
+		if (condition)
+			let i = 0;
+		```
+		 */
+		throw new TypecheckingFailure("Declarations can only be declared inside a block", node.thenStatement);
+	}
 
 	env.enterScope();
 	const thenBlock: StatementReturn = await TypeChecker.accept(node.thenStatement, env);
