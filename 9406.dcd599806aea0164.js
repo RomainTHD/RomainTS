3b79d7e5f6b6fe425abbfe07bdd6e369d0f70d75
RomainTHD <romain-thd+github@protonmail.com>
2023-08-08 12:40:07 +0000

    ci: Auto-deploy to GitHub Pages
    Triggered by commit: https://github.com/RomainTHD/RomainTS/commit/6778ab39f9f5c1698418bd87f8b2fa7d65031874

---
+++ 9406.dcd599806aea0164.js
@@ -0,0 +1,53 @@
+"use strict";
+(self["webpackChunkromaints"] = self["webpackChunkromaints"] || []).push([[9406],{
+
+/***/ 9406:
+/*!**********************************************************************!*\
+  !*** ../src/typechecker/visitor/expression/CallExpressionVisitor.ts ***!
+  \**********************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   visit: () => (/* binding */ visit)
+/* harmony export */ });
+/* harmony import */ var _home_runner_work_RomainTS_RomainTS_demo_node_modules_babel_runtime_helpers_esm_asyncToGenerator_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./node_modules/@babel/runtime/helpers/esm/asyncToGenerator.js */ 5861);
+/* harmony import */ var ___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../.. */ 2429);
+/* harmony import */ var _types__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../types */ 2122);
+
+
+
+const visit = /*#__PURE__*/function () {
+  var _ref = (0,_home_runner_work_RomainTS_RomainTS_demo_node_modules_babel_runtime_helpers_esm_asyncToGenerator_js__WEBPACK_IMPORTED_MODULE_0__["default"])(function* (node, env) {
+    const left = yield ___WEBPACK_IMPORTED_MODULE_1__.TypeChecker.accept(node.expression, env);
+    if (!(left.eType instanceof _types__WEBPACK_IMPORTED_MODULE_2__.FunctionType)) {
+      throw new ___WEBPACK_IMPORTED_MODULE_1__.TypecheckingFailure(`Cannot call non-function type '${left.eType}'`, node);
+    }
+    const f = left.eType;
+    const args = [];
+    for (const arg of node.arguments) {
+      args.push((yield ___WEBPACK_IMPORTED_MODULE_1__.TypeChecker.accept(arg, env)).eType);
+    }
+    if (f.params.length !== args.length) {
+      // TODO: Handle optional parameters
+      throw new ___WEBPACK_IMPORTED_MODULE_1__.TypecheckingFailure(`Expected ${f.params.length} arguments, got ${args.length}`, node);
+    }
+    for (let i = 0; i < args.length; i++) {
+      const param = f.params[i];
+      const arg = args[i];
+      if (!param.pType.contains(arg)) {
+        throw new ___WEBPACK_IMPORTED_MODULE_1__.TypecheckingFailure(`Argument ${i} has type '${arg}', but expected '${param.pType}'`, node);
+      }
+    }
+    return {
+      eType: f.retType
+    };
+  });
+  return function visit(_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+/***/ })
+
+}]);
